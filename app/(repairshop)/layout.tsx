686f2a9b1be5a748c98d183b12da7e8381d139a9
Kwandile Mgenge <kwandilen.mgenge@gmail.com>
2026-09-07 12:14:48 +0200

    Configured Routing Group, Layouts and Templates

---
+++ app/(repairshop)/layout.tsx
@@ -0,0 +1,12 @@
+export default async function RSLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div>
+      {/* Header */}
+      <div>{children}</div>
+    </div>
+  );
+}
---
+++ app/(repairshop)/template.tsx
@@ -0,0 +1,9 @@
+export default async function RSLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="animate-appear">{children}</div>
+  );
+}
