02a7f8f1385fd280d3a99ce663deeb09114bdbdd
Kwandile Mgenge <kwandilen.mgenge@gmail.com>
2026-09-18 03:52:24 +0200

    Installed and created safe action client

---
+++ lib/safe-action.ts
@@ -0,0 +1,30 @@
+import { createSafeActionClient } from "next-safe-action";
+import { z } from "zod";
+import * as Sentry from "@sentry/nextjs";
+
+export const actionClient = createSafeActionClient({
+  defineMetadataSchema() {
+    return z.object({
+      userId: z.string().optional(),
+      actionName: z.string(),
+    });
+  },
+  handleServerError(e, utils) {
+    const { clientInput, metadata } = utils;
+    
+    Sentry.captureException(e, (scope) => {
+      scope.setContext('serverError', { message: e.message });
+      
+      scope.setContext('metadata', { actionName: metadata?.actionName });
+      scope.setContext('clientInput', { clientInput });
+      
+      return scope;
+    });
+
+    if (e.constructor.name === "DatabaseError") {
+      return 'Database error occurred. Please try again later.';
+    }
+    
+    return e.message || 'An unexpected error occurred. Please try again later.';
+  }
+});
